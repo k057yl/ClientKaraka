541b0f19efc65ec2a480d68cf488ed18f651aba4
Roman Kostukov <romank057yl@gmail.com>
2025-11-21 18:59:27 +0200

    AddedIndex

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.karakatsiya.com/api',
+  apiBaseUrl: 'https://apikaraka.onrender.com/api',
   cloudinaryUrl: 'https://api.cloudinary.com/v1_1/drne5mbi9/image/upload',
   cloudinaryPreset: 'drne5mbi9'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7280/api',
+  apiBaseUrl: 'https://apikaraka.onrender.com/api',
   cloudinaryUrl: 'https://api.cloudinary.com/v1_1/drne5mbi9/image/upload',
   cloudinaryPreset: 'drne5mbi9'
 };
